ec137e93f710afda67777508f20a12d27fc7f6a1
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-06-01 13:04:40 +0200

    minor improvements..

--- include/location.h
+++ include/location.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "memory.h"
+#include "json.h"
 
 namespace coco
 {
@@ -10,4 +11,6 @@ namespace coco
   };
 
   using location_ptr = utils::u_ptr<location>;
+
+  inline json::json to_json(const location &l) { return {{"x", l.x}, {"y", l.y}}; }
 } // namespace coco

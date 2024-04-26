71093b004d9acb67557c5e1d11a242d8550b690a
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-04-26 09:15:25 +0200

    Refactor logging statements in coco_core.cpp to use [[maybe_unused]] for unused function parameters

--- src/coco_core.cpp
+++ src/coco_core.cpp
@@ -10,14 +10,14 @@ namespace coco
         assert(env != nullptr);
     }
 
-    void coco_core::new_sensor_type(const sensor_type &s) { LOG_TRACE("New sensor type: " + s.get_id()); }
-    void coco_core::updated_sensor_type(const sensor_type &s) { LOG_TRACE("Updated sensor type: " + s.get_id()); }
-    void coco_core::deleted_sensor_type(const std::string &id) { LOG_TRACE("Deleted sensor type: " + id); }
+    void coco_core::new_sensor_type([[maybe_unused]] const sensor_type &s) { LOG_TRACE("New sensor type: " + s.get_id()); }
+    void coco_core::updated_sensor_type([[maybe_unused]] const sensor_type &s) { LOG_TRACE("Updated sensor type: " + s.get_id()); }
+    void coco_core::deleted_sensor_type([[maybe_unused]] const std::string &id) { LOG_TRACE("Deleted sensor type: " + id); }
 
-    void coco_core::new_sensor(const sensor &s) { LOG_TRACE("New sensor: " + s.get_id()); }
-    void coco_core::updated_sensor(const sensor &s) { LOG_TRACE("Updated sensor: " + s.get_id()); }
-    void coco_core::deleted_sensor(const std::string &id) { LOG_TRACE("Deleted sensor: " + id); }
+    void coco_core::new_sensor([[maybe_unused]] const sensor &s) { LOG_TRACE("New sensor: " + s.get_id()); }
+    void coco_core::updated_sensor([[maybe_unused]] const sensor &s) { LOG_TRACE("Updated sensor: " + s.get_id()); }
+    void coco_core::deleted_sensor([[maybe_unused]] const std::string &id) { LOG_TRACE("Deleted sensor: " + id); }
 
-    void coco_core::new_sensor_value(const sensor &s, const std::chrono::system_clock::time_point &timestamp, const json::json &value) { LOG_TRACE("Sensor " + s.get_id() + " value: " + value.to_string()); }
-    void coco_core::new_sensor_state(const sensor &s, const std::chrono::system_clock::time_point &timestamp, const json::json &state) { LOG_TRACE("Sensor " + s.get_id() + " state: " + state.to_string()); }
+    void coco_core::new_sensor_value([[maybe_unused]] const sensor &s, [[maybe_unused]] const std::chrono::system_clock::time_point &timestamp, [[maybe_unused]] const json::json &value) { LOG_TRACE("Sensor " + s.get_id() + " value: " + value.to_string()); }
+    void coco_core::new_sensor_state([[maybe_unused]] const sensor &s, [[maybe_unused]] const std::chrono::system_clock::time_point &timestamp, [[maybe_unused]] const json::json &state) { LOG_TRACE("Sensor " + s.get_id() + " state: " + state.to_string()); }
 } // namespace coco

3d35acbadeecfc7c515c6bd1c22f92073e6aa8b6
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-05-03 17:09:49 +0200

    Refactor mongo_db constructor to include default config values

--- include/mongo/mongo_db.hpp
+++ include/mongo/mongo_db.hpp
@@ -11,7 +11,7 @@ namespace coco
   class mongo_db : public coco_db
   {
   public:
-    mongo_db(const json::json &config = {}, const std::string &mongodb_uri = MONGODB_URI(MONGODB_HOST, MONGODB_PORT));
+    mongo_db(const json::json &config = {{"name", COCO_NAME}}, const std::string &mongodb_uri = MONGODB_URI(MONGODB_HOST, MONGODB_PORT));
     virtual ~mongo_db() = default;
 
     sensor_type &create_sensor_type(const std::string &name, const std::string &description, std::vector<std::unique_ptr<parameter>> &&pars) override;
@@ -24,11 +24,11 @@ namespace coco
     mongocxx::client conn;
 
   protected:
-    mongocxx::v_noabi::database db;
+    mongocxx::database db;
 
   private:
-    mongocxx::v_noabi::collection sensor_types_collection;
-    mongocxx::v_noabi::collection sensors_collection;
-    mongocxx::v_noabi::collection sensor_data_collection;
+    mongocxx::collection sensor_types_collection;
+    mongocxx::collection sensors_collection;
+    mongocxx::collection sensor_data_collection;
   };
 } // namespace coco

1910b7169dc804aa1bfd8a839ffd36fd3651bbbf
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-03-08 10:38:38 +0100

    Update CMake configuration for MongoDB and JSON submodule integration

--- tests/test_coco.cpp
+++ tests/test_coco.cpp
@@ -1,11 +1,11 @@
 #include "coco.hpp"
-#ifdef ENABLE_MONGO_DB
+#ifdef BUILD_MONGODB
 #include "mongo_db.hpp"
 #endif
 
 int main()
 {
-#ifdef ENABLE_MONGO_DB
+#ifdef BUILD_MONGODB
     mongocxx::instance inst{}; // This should be done only once.
     coco::mongo_db db;
 #endif

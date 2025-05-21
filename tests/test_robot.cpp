c037733da7cdd0e8303c0100dc3ded19919a7ffb
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-05-21 14:38:30 +0200

    Add robot_tests executable and enhance test coverage; modify main function signature in test_coco.cpp

--- tests/test_robot.cpp
+++ tests/test_robot.cpp
@@ -61,47 +61,30 @@ int main()
 #endif
 
 #ifdef BUILD_LLM
+    llm.create_intent("affirmative", "The user agrees with the system");
+    llm.create_intent("negative", "The user disagrees with the system");
     llm.create_intent("greet", "The user greets the system");
-    llm.create_intent("bye", "The user says goodbye to the system");
+    llm.create_intent("farewell", "The user says goodbye to the system");
     llm.create_entity(coco::string_type, "user_name", "The name of the user");
     llm.create_entity(coco::integer_type, "user_age", "The age of the user");
-    llm.create_entity(coco::string_type, "robot_response", "A message in italian answering, commenting or requesting for clarification to the user's input, in the same tone used by the user. Try to be creative and funny.");
+    llm.create_entity(coco::string_type, "robot_response", "A message in italian answering, commenting or requesting for clarification to the user's input, in the same tone used by the user. Try to be creative and funny. Always set this entity.");
+    llm.create_entity(coco::string_type, "robot_face", "The face of the robot. Set this entity to a string among the following: 'happy', 'sad', 'angry', 'surprised', 'neutral'. This entity is used to set the face of the robot. Always set this entity.");
+    llm.create_entity(coco::boolean_type, "robot_ask", "Set this entity to true if the robot is asking a question. Set this entity to false if the robot is not asking a question. Always set this entity.");
     llm.create_slot(coco::string_type, "user_name", "The name of the user");
     llm.create_slot(coco::integer_type, "user_age", "The age of the user");
     cc.create_reactive_rule("set_name", "(defrule set_name ?f <- (entity (item_id ?id) (name user_name) (value ?value)) => (set_slots ?id (create$ user_name) (create$ ?value)) (retract ?f))");
     cc.create_reactive_rule("set_age", "(defrule set_age ?f <- (entity (item_id ?id) (name user_age) (value ?value)) => (set_slots ?id (create$ user_age) (create$ ?value)) (retract ?f))");
 #endif
 
-    auto &tp = cc.create_type("ParentType", {}, json::json(), json::json{{"online", {"type", "bool"}}});
-    auto &itm_0 = cc.create_item(tp);
-    cc.set_value(itm_0, json::json{{"online", true}});
-    auto &itm_1 = cc.create_item(tp);
-    cc.set_value(itm_1, json::json{{"online", false}});
+    auto &user_tp = cc.create_type("User", {}, json::json{{"name", {"type", "string"}}, {"age", {"type", "int"}}}, {});
+    [[maybe_unused]] auto &user_itm = cc.create_item(user_tp);
 
-    auto &ch_tp = cc.create_type("ChildType", {tp}, json::json(), json::json{{"count", {{"type", "int"}, {"min", 0}}}, {"temp", {{"type", "float"}, {"max", 50}}}});
-    auto &ch_itm = cc.create_item(ch_tp);
-    cc.set_value(ch_itm, json::json{{"online", true}, {"count", 1}, {"temp", 22.5}});
-
-    auto &s_tp = cc.create_type("SourceType", {}, json::json(), json::json{{"parent", {{"type", "item"}, {"domain", "ParentType"}}}});
-    auto &s_itm = cc.create_item(s_tp);
-    cc.set_value(s_itm, json::json{{"parent", ch_itm.get_id().c_str()}});
-
-    std::string user_input;
-    do
-    {
-        std::cout << "Enter a command (d to drop the database, q to quit): ";
-        std::getline(std::cin, user_input);
-        if (user_input == "d")
-            db.drop();
-#ifdef BUILD_LLM
-        else
-            llm.understand(itm_0, user_input);
-#endif
-    } while (user_input != "d" && user_input != "q");
+    auto &robot_tp = cc.create_type("Robot", {}, {}, json::json{{"saying", {"type", "string"}}, {"robot_face", {{"type", "symbol"}, {"values", {"happy", "sad", "angry", "surprised", "neutral"}}}}, {"user", {{"type", "item"}, {"domain", "User"}}}, {"listening", {"type", "bool"}}});
+    [[maybe_unused]] auto &robot_itm = cc.create_item(robot_tp);
 
 #ifdef BUILD_SERVER
+    std::this_thread::sleep_for(std::chrono::seconds(100));
+    db.drop();
     srv.stop();
 #endif
-
-    return 0;
 }

4fcdcdbcd2ad32db241b6e46e6e857b1582fd754
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-09-01 12:52:38 +0200

    chore: update @ratiosolver/coco to version 0.0.50 and add buffer dependency
    - Bump @ratiosolver/coco version from 0.0.49 to 0.0.50 in package.json and package-lock.json.
    - Add buffer as a dependency in @ratiosolver/coco package.json.
    - Update index.ts to declare and assign Buffer to the window object for global access.

--- gui/coco/src/index.ts
+++ gui/coco/src/index.ts
@@ -1,5 +1,13 @@
 import { ComputePositionConfig } from '@floating-ui/dom';
 
+import { Buffer } from 'buffer';
+declare global {
+  interface Window {
+    Buffer: typeof Buffer;
+  }
+}
+window.Buffer = Buffer;
+
 declare module 'cytoscape-popper' {
 
   interface PopperOptions extends ComputePositionConfig {

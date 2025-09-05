f6be75fe8cd6e566528b2ba1cf57be19bed7e294
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-09-05 11:55:24 +0200

    feat: enhance CoCo components and update peer dependencies

--- gui/app/src/main.ts
+++ gui/app/src/main.ts
@@ -1,26 +1,46 @@
-import { AppComponent, BrandComponent, Connection, LogInButton, LogInModal, LogOutButton, NewUserButton, NewUserModal, Settings } from '@ratiosolver/flick';
-import { coco, Offcanvas } from '@ratiosolver/coco';
+import { AppComponent, BrandComponent, Connection, LogInButton, LogInModal, LogOutButton, NavbarContent, NewUserButton, NewUserModal, Settings } from '@ratiosolver/flick';
+import { coco, Offcanvas as CoCoOffcanvas } from '@ratiosolver/coco';
 import './styles.css'
+import { Offcanvas } from 'bootstrap';
 
 Settings.get_instance().load_settings({ ws_path: '/coco' });
 
 const has_users = false;
 const offcanvas_id = 'coco-offcanvas';
 
-class CoCoApp extends AppComponent {
+class CoCoBrandComponent extends BrandComponent {
 
+  constructor() {
+    super('CoCo', 'favicon.ico', 32, 32);
+    this.node.id = 'coco-brand';
+  }
+}
+
+class CoCoNavbarContent extends NavbarContent {
   constructor() {
     super();
 
-    // Create and add brand element
-    this.navbar.add_child(new BrandComponent('CoCo', 'favicon.ico', 32, 32, offcanvas_id));
+    if (has_users) {
+      this.add_child(new NewUserButton());
+      this.add_child(new LogInButton());
+      this.add_child(new LogOutButton());
+    }
+  }
+}
 
-    this.add_child(new Offcanvas(offcanvas_id));
+class CoCoApp extends AppComponent {
+
+  constructor() {
+    super(new CoCoBrandComponent(), new CoCoNavbarContent());
+
+    const offcanvas = new CoCoOffcanvas(offcanvas_id);
+    this.add_child(offcanvas);
+    document.getElementById('coco-brand')?.addEventListener('click', (event) => {
+      event.preventDefault();
+      Offcanvas.getOrCreateInstance(document.getElementById(offcanvas_id)!).toggle();
+    });
 
     if (has_users) {
-      this.navbar.add_child(new NewUserButton());
-      this.navbar.add_child(new LogInButton());
-      this.navbar.add_child(new LogOutButton());
       this.add_child(new NewUserModal());
       this.add_child(new LogInModal());
       const token = localStorage.getItem('token');

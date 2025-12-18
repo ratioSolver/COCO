527431f9bb105ec16475b8ad0b85d854e8327455
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-12-18 11:31:02 +0100

    feat: enhance ROS interface generation and update test setup

--- ros/main.cpp
+++ ros/main.cpp
@@ -1,4 +1,25 @@
-int main()
+#include "logging.hpp"
+#include <fstream>
+
+int main(int argc, char const *argv[])
 {
+    std::string module_name;
+    for (int i = 1; i < argc; ++i)
+        if (std::string(argv[i]) == "-o" && i + 1 < argc)
+        {
+            module_name = argv[i + 1];
+            break;
+        }
+
+    LOG_INFO("Generating coco_ros.hpp");
+    std::ofstream header_out(module_name + "/coco_ros.hpp", std::ios::out | std::ios::trunc);
+    if (!header_out)
+    {
+        LOG_ERR("Cannot open output file: coco_ros.hpp");
+        return 1;
+    }
+    header_out << "#pragma once\n\n";
+    header_out << "#include \"coco_module.hpp\"\n";
+    header_out << "#include \"rclcpp/rclcpp.hpp\"\n\n";
     return 0;
 }
--- tests/ros/test_ros.cpp
+++ tests/ros/test_ros.cpp
@@ -1,3 +1,5 @@
+#include "coco_ros.hpp"
+
 int main()
 {
     return 0;

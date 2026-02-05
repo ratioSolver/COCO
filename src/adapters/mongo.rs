484a65bcb1abbfb82257fc51b7960dc8bf4184fc
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-05 18:33:40 +0100

    feat: Enhance MongoDBDataStore initialization with index creation and update server entry point

--- src/adapters/mongo.rs
+++ src/adapters/mongo.rs
@@ -1,11 +1,34 @@
 use crate::DataStore;
 use async_trait::async_trait;
+use mongodb::bson::doc;
+use mongodb::error::Error;
+use mongodb::options::IndexOptions;
+use mongodb::{Client, IndexModel};
 
-pub struct MongoDBDataStore {}
+pub struct MongoDBDataStore {
+    name: String,
+    client: Client,
+}
 
 impl MongoDBDataStore {
-    pub fn new() -> Self {
-        MongoDBDataStore {}
+    pub async fn new(name: &str, connection_string: &str) -> Result<Self, Error> {
+        let client = Client::with_uri_str(connection_string).await?;
+        let db = client.database(name);
+        let collection_names = db.list_collection_names().await?;
+        if collection_names.is_empty() {
+            let classes_collection = db.collection::<mongodb::bson::Document>("classes");
+            let index = IndexModel::builder().keys(doc! { "name": 1 }).options(IndexOptions::builder().unique(true).build()).build();
+            classes_collection.create_index(index).await?;
+
+            let rules_collection = db.collection::<mongodb::bson::Document>("rules");
+            let index = IndexModel::builder().keys(doc! { "name": 1 }).options(IndexOptions::builder().unique(true).build()).build();
+            rules_collection.create_index(index).await?;
+
+            let object_data_collection = db.collection::<mongodb::bson::Document>("object_data");
+            let index = IndexModel::builder().keys(doc! { "object_id": 1, "timestamp": 1 }).options(IndexOptions::builder().unique(true).build()).build();
+            object_data_collection.create_index(index).await?;
+        }
+        Ok(Self { name: name.to_string(), client })
     }
 }
 
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
-    let db = Arc::new(MongoDBDataStore {});
+    let db = Arc::new(MongoDBDataStore::new("coco_server", "mongodb://localhost:27017").await.unwrap());
     let kb = Arc::new(Mutex::new(CLIPSKnowledgeBase::new()));
     let coco = CoCo::new(db, kb).await;
 }

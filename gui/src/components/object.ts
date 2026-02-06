9a765b1e0505da41ec4776e410f7a986c1b74083
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-06 15:16:39 +0100

    feat: Update Object component layout and improve input handling for better user experience

--- gui/src/components/object.ts
+++ gui/src/components/object.ts
@@ -11,12 +11,13 @@ export function ObjectsList(coco: coco.CoCo): VNode {
 }
 
 export function Object(obj: coco.CoCoObject): VNode {
-  const content = h('div.container.mt-5.text-center', [
-    h('div', [
-      h('input.form-control', { attrs: { type: 'text', value: obj.get_id(), placeholder: 'Object ID', readonly: 'readonly' } }),
+  const content = h('div.container.mt-2.text-center', [
+    h('div.input-group', [
+      h('input.form-control', { attrs: { type: 'text', value: obj.get_id(), placeholder: 'Type name', disabled: true } }),
       h('button.btn.btn-outline-secondary', {
+        attrs: { type: 'button', title: 'Copy type name to clipboard' },
         on: { click: () => navigator.clipboard.writeText(obj.get_id()) }
-      }, h('i.fa-regular.fa-copy.me-2')),
+      }, h('i.fa-solid.fa-copy')),
     ]),
   ]);
   return content;

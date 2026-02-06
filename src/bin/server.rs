6c71522b5dad74929cf93836c971e5e1152823bf
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-06 05:38:51 +0100

    feat: Enhance CoCo functionality with improved class and object creation, error handling, and new methods for rules management

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -8,10 +8,10 @@ use axum::{
     response::IntoResponse,
     routing::get,
 };
-use coco::{CLIPSKnowledgeBase, Class, CoCo, MongoDBDataStore, Object};
+use coco::{CLIPSKnowledgeBase, Class, CoCo, CoCoEvent, MongoDBDataStore, Object};
 use std::sync::{Arc, Mutex};
 use tokio::sync::broadcast;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 use utoipa::OpenApi;
 
 #[tokio::main]
@@ -80,11 +80,14 @@ async fn get_class(Path(name): Path<String>, State(coco): State<Arc<CoCo>>) -> i
         request_body = Class,
         responses(
             (status = 201, description = "Class created successfully"),
+            (status = 500, description = "Failed to create class")
         )
     )]
 async fn create_class(State(coco): State<Arc<CoCo>>, axum::Json(class): axum::Json<Class>) -> impl IntoResponse {
-    coco.create_class(&class).await;
-    StatusCode::CREATED
+    match coco.create_class(class).await {
+        Ok(_) => StatusCode::CREATED.into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to create class: {}", e)).into_response(),
+    }
 }
 
 #[utoipa::path(
@@ -131,11 +134,14 @@ async fn get_object(Path(id): Path<String>, State(coco): State<Arc<CoCo>>) -> im
         request_body = Object,
         responses(
             (status = 201, description = "Object created successfully"),
+            (status = 500, description = "Failed to create object")
         )
     )]
 async fn create_object(State(coco): State<Arc<CoCo>>, axum::Json(object): axum::Json<Object>) -> impl IntoResponse {
-    coco.create_object(object.classes, object.properties, object.values).await;
-    StatusCode::CREATED
+    match coco.create_object(object).await {
+        Ok(_) => StatusCode::CREATED.into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to create object: {}", e)).into_response(),
+    }
 }
 
 #[utoipa::path(
@@ -177,16 +183,57 @@ async fn handle_socket(mut socket: WebSocket, coco: Arc<CoCo>) {
             (name, v)
         })
         .collect();
+    let objects_map: std::collections::HashMap<String, serde_json::Value> = coco
+        .get_objects()
+        .into_iter()
+        .map(|o| {
+            let id = o.id.clone();
+            let mut v = serde_json::to_value(o).unwrap();
+            v.as_object_mut().unwrap().remove("id");
+            (id, v)
+        })
+        .collect();
+    let rules_map: std::collections::HashMap<String, serde_json::Value> = coco
+        .get_rules()
+        .into_iter()
+        .map(|r| {
+            let name = r.name.clone();
+            let mut v = serde_json::to_value(r).unwrap();
+            v.as_object_mut().unwrap().remove("name");
+            (name, v)
+        })
+        .collect();
     let init_msg = serde_json::json!({
         "msg_type": "coco",
-        "classes": classes_map
+        "classes": classes_map,
+        "objects": objects_map,
+        "rules": rules_map
     });
     socket.send(Message::Text(serde_json::to_string(&init_msg).unwrap().into())).await.unwrap();
 
-    let mut rx = coco.lock().unwrap().get_event_sender().subscribe();
+    let mut rx = coco.get_event_sender().subscribe();
     while let Ok(msg) = rx.recv().await {
         println!("Sending WebSocket message: {:?}", msg);
-        socket.send(Message::Text(serde_json::to_string(&msg).unwrap().into())).await.unwrap();
+        match msg {
+            CoCoEvent::ClassCreated(class) => {
+                let mut update_msg = serde_json::to_value(&class).unwrap();
+                update_msg.as_object_mut().unwrap().insert("msg_type".to_string(), serde_json::json!("class_created"));
+                socket.send(Message::Text(serde_json::to_string(&update_msg).unwrap().into())).await.unwrap();
+            }
+            CoCoEvent::ObjectCreated(object) => {
+                let mut update_msg = serde_json::to_value(&object).unwrap();
+                update_msg.as_object_mut().unwrap().insert("msg_type".to_string(), serde_json::json!("object_created"));
+                socket.send(Message::Text(serde_json::to_string(&update_msg).unwrap().into())).await.unwrap();
+            }
+            CoCoEvent::AddedValues(object_id, values) => {
+                let update_msg = serde_json::json!({
+                    "msg_type": "added_values",
+                    "object_id": object_id,
+                    "values": values
+                });
+                socket.send(Message::Text(serde_json::to_string(&update_msg).unwrap().into())).await.unwrap();
+            }
+        }
     }
 }
 
--- src/coco.rs
+++ src/coco.rs
@@ -1,6 +1,8 @@
-use crate::{Class, CoCoEvent, DataStore, KnowledgeBase, Object, Property, Rule};
+use crate::{Class, CoCoEvent, DataStore, KnowledgeBase, Object, Property, Rule, Value};
+use chrono::{DateTime, Utc};
 use std::{
     collections::{HashMap, HashSet},
+    error::Error,
     sync::{Arc, Mutex, RwLock},
 };
 use tokio::sync::broadcast;
@@ -43,22 +45,104 @@ impl CoCo {
         self.sender.clone()
     }
 
-    pub async fn create_new_class(&self, name: &str, parents: Option<HashSet<String>>, static_properties: Option<HashMap<String, Property>>, dynamic_properties: Option<HashMap<String, Property>>) {
-        let class = Class { name: name.to_string(), parents, static_properties, dynamic_properties };
+    pub fn get_classes(&self) -> Vec<Class> {
+        self.classes.read().unwrap().values().cloned().collect()
+    }
+
+    pub fn get_class(&self, name: &str) -> Option<Class> {
+        self.classes.read().unwrap().get(name).cloned()
+    }
+
+    pub async fn create_new_class(&self, name: &str, parents: Option<HashSet<String>>, static_properties: Option<HashMap<String, Property>>, dynamic_properties: Option<HashMap<String, Property>>) -> Result<(), Box<dyn Error>> {
+        self.create_class(Class { name: name.to_string(), parents, static_properties, dynamic_properties }).await
+    }
+
+    pub async fn create_class(&self, class: Class) -> Result<(), Box<dyn Error>> {
         self.db.create_class(&class).await.expect("Failed to create class in database");
-        self.add_classes(vec![class]);
+        self.add_classes(vec![class])?;
+        Ok(())
     }
 
-    pub async fn create_class(&self, class: &Class) {
-        self.db.create_class(class).await.expect("Failed to create class in database");
-        self.add_classes(vec![class.clone()]);
-    }
-
-    fn add_classes(&self, classes: Vec<Class>) {
-        for class in &classes {
-            self.kb.lock().unwrap().create_class(class);
+    fn add_classes(&self, classes: Vec<Class>) -> Result<(), Box<dyn Error>> {
+        for class in classes {
+            self.kb.lock().unwrap().create_class(&class)?;
             self.classes.write().unwrap().insert(class.name.clone(), class.clone());
-            self.sender.send(CoCoEvent::ClassCreated(class.clone())).expect("Failed to send ClassCreated event");
+            self.sender.send(CoCoEvent::ClassCreated(class)).expect("Failed to send ClassCreated event");
         }
+        Ok(())
+    }
+
+    pub fn get_objects(&self) -> Vec<Object> {
+        self.objects.read().unwrap().values().cloned().collect()
+    }
+
+    pub fn get_object(&self, id: &str) -> Option<Object> {
+        self.objects.read().unwrap().get(id).cloned()
+    }
+
+    pub async fn create_new_object(&self, classes: HashSet<String>, properties: Option<HashMap<String, Value>>, values: Option<HashMap<String, (Value, DateTime<Utc>)>>) -> Result<(), Box<dyn Error>> {
+        self.create_object(Object { id: String::new(), classes, properties, values }).await
+    }
+
+    pub async fn create_object(&self, object: Object) -> Result<(), Box<dyn Error>> {
+        let id = self.db.create_object(&object).await?;
+        let object = Object { id, ..object };
+        self.add_objects(vec![object])?;
+        Ok(())
+    }
+
+    fn add_objects(&self, objects: Vec<Object>) -> Result<(), Box<dyn Error>> {
+        let class_guard = self.classes.read().unwrap();
+        for object in objects {
+            for class_name in &object.classes {
+                let class = class_guard.get(class_name).expect("Class not found for object");
+                self.kb.lock().unwrap().create_object(&class, &object).expect("Failed to create object in knowledge base");
+            }
+            self.objects.write().expect("Failed to lock objects for writing").insert(object.id.clone(), object.clone());
+            self.sender.send(CoCoEvent::ObjectCreated(object)).expect("Failed to send ObjectCreated event");
+        }
+        Ok(())
+    }
+
+    pub async fn set_values(&self, object: &mut Object, values: &HashMap<String, Value>) -> Result<(), Box<dyn Error>> {
+        let date_time: DateTime<Utc> = Utc::now();
+        self.db.set_values(&object, &values, &date_time).await?;
+        let class_guard = self.classes.read().unwrap();
+        for class_name in &object.classes {
+            let class = class_guard.get(class_name).expect("Class not found for object");
+            self.kb.lock().unwrap().add_data(&class, &object, &values, &date_time)?;
+        }
+        Ok(())
+    }
+
+    pub fn get_rules(&self) -> Vec<Rule> {
+        self.rules.read().unwrap().values().cloned().collect()
+    }
+
+    pub fn get_rule(&self, name: &str) -> Option<Rule> {
+        self.rules.read().unwrap().get(name).cloned()
+    }
+
+    pub async fn create_new_rule(&self, name: &str, content: &str) -> Result<(), Box<dyn Error>> {
+        self.create_rule(Rule { name: name.to_string(), content: content.to_string() }).await
+    }
+
+    pub async fn create_rule(&self, rule: Rule) -> Result<(), Box<dyn Error>> {
+        self.db.create_rule(&rule).await?;
+        self.add_rules(vec![rule])?;
+        Ok(())
+    }
+
+    fn add_rules(&self, db_rules: Vec<Rule>) -> Result<(), Box<dyn Error>> {
+        for rule in db_rules {
+            self.kb.lock().unwrap().create_rule(&rule)?;
+            self.rules.write().unwrap().insert(rule.name.clone(), rule);
+        }
+        Ok(())
+    }
+
+    pub async fn drop_db(&self) -> Result<(), Box<dyn Error>> {
+        self.db.drop_db().await?;
+        Ok(())
     }
 }

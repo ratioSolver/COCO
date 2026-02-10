8e4507287d7ade5ac12d70e04dae3bca2bbbe6d3
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-10 14:31:00 +0100

    feat: Enhance CLIPSKnowledgeBase with environment initialization and UDF registration

--- src/kb/clips.rs
+++ src/kb/clips.rs
@@ -1,10 +1,16 @@
 use crate::kb::KnowledgeBase;
+use clips::{ClipsValue, Environment, Type};
 
-pub struct CLIPSKnowledgeBase {}
+pub struct CLIPSKnowledgeBase {
+    env: Environment,
+}
 
 impl CLIPSKnowledgeBase {
     pub fn new() -> Self {
-        Self {}
+        let mut env = Environment::new().expect("Failed to create CLIPS environment");
+        env.add_udf("add-data", None, 3, 4, vec![Type(Type::SYMBOL), Type(Type::MULTIFIELD), Type(Type::MULTIFIELD), Type(Type::INTEGER)], |_env, _ctx| ClipsValue::Void()).expect("Failed to add UDF to CLIPS environment");
+        env.add_udf("add-class", None, 2, 2, vec![Type(Type::SYMBOL), Type(Type::SYMBOL)], |_env, _ctx| ClipsValue::Void()).expect("Failed to add UDF to CLIPS environment");
+        Self { env }
     }
 }
 

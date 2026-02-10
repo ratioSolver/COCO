61c215f4657ac8a7f1df4eeeff91933edd7a6664
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-10 16:42:36 +0100

    feat: Implement CLIPSKnowledgeBase with class management and UDF registration

--- src/kb/clips.rs
+++ src/kb/clips.rs
@@ -1,8 +1,13 @@
-use crate::kb::KnowledgeBase;
+use crate::{
+    kb::{KnowledgeBase, KnowledgeBaseError},
+    model::Class,
+};
 use clips::{ClipsValue, Environment, Type};
+use std::collections::HashMap;
 
 pub struct CLIPSKnowledgeBase {
     env: Environment,
+    classes: HashMap<String, Class>,
 }
 
 impl CLIPSKnowledgeBase {
@@ -10,8 +15,24 @@ impl CLIPSKnowledgeBase {
         let mut env = Environment::new().expect("Failed to create CLIPS environment");
         env.add_udf("add-data", None, 3, 4, vec![Type(Type::SYMBOL), Type(Type::MULTIFIELD), Type(Type::MULTIFIELD), Type(Type::INTEGER)], |_env, _ctx| ClipsValue::Void()).expect("Failed to add UDF to CLIPS environment");
         env.add_udf("add-class", None, 2, 2, vec![Type(Type::SYMBOL), Type(Type::SYMBOL)], |_env, _ctx| ClipsValue::Void()).expect("Failed to add UDF to CLIPS environment");
-        Self { env }
+        Self { env, classes: HashMap::new() }
     }
 }
 
-impl KnowledgeBase for CLIPSKnowledgeBase {}
+impl KnowledgeBase for CLIPSKnowledgeBase {
+    fn get_classes(&self) -> Vec<&Class> {
+        self.classes.values().collect()
+    }
+
+    fn get_class(&self, name: &str) -> Option<&Class> {
+        self.classes.get(name)
+    }
+
+    fn create_class(&mut self, class: &Class) -> Result<(), KnowledgeBaseError> {
+        if self.classes.contains_key(&class.name) {
+            return Err(KnowledgeBaseError::ClassAlreadyExists(class.name.clone()));
+        }
+        self.classes.insert(class.name.clone(), class.clone());
+        Ok(())
+    }
+}
--- src/kb/mod.rs
+++ src/kb/mod.rs
@@ -1,4 +1,16 @@
-pub trait KnowledgeBase {}
+use crate::model::Class;
+
+#[derive(Debug)]
+pub enum KnowledgeBaseError {
+    ClassAlreadyExists(String),
+    ClassNotFound(String),
+}
+
+pub trait KnowledgeBase {
+    fn get_classes(&self) -> Vec<&Class>;
+    fn get_class(&self, name: &str) -> Option<&Class>;
+    fn create_class(&mut self, class: &Class) -> Result<(), KnowledgeBaseError>;
+}
 
 #[cfg(feature = "clips")]
 pub mod clips;

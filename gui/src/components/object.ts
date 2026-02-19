bfca54d8ac3478bf8418204beb9489dffca8441d
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-02-19 15:25:40 +0100

    feat: enhance chart rendering and update listener logic in CoCoObject

--- gui/src/components/object.ts
+++ gui/src/components/object.ts
@@ -48,35 +48,75 @@ export function ObjectsList(coco: coco.CoCo): VNode {
   return ListGroup(Array.from(coco.get_objects().values().map(obj => ObjectGroupItem(obj))));
 }
 
-const obj_listener = {
-  class_added: (_cls: coco.CoCoClass) => { flick.redraw(); },
-  properties_updated: (_properties: Record<string, coco.Value>) => { flick.redraw(); },
-  values_added: (_values: Record<string, coco.Value>, _date_time: string) => { },
-  data_updated: (_data: Record<string, Array<coco.TimeValue>>) => { }
-};
-
-let chart: ObjectChart | null = null;
-
 export function CoCoObject(obj: coco.CoCoObject): VNode {
   const props_header = ["Property", "Value"];
   const props = obj.get_properties();
   const props_rows = props ? Object.entries(props).map(([name, value]) => Row([name, coco.value_to_string(value)])) : [];
   const data = obj.get_data();
-  if (Object.keys(data).length === 0)
+  const num_layers = Object.keys(data).length;
+  if (num_layers === 0)
     obj.load_data();
 
-  const vals = obj.get_values();
-  const content = h('div.container.mt-2',
-    {
-      hook: {
-        insert: () => {
-          obj.add_listener(obj_listener);
-        },
-        destroy: () => {
-          obj.remove_listener(obj_listener);
-        }
+  let chart: echarts.ECharts | undefined;
+
+  const get_option = (): echarts.EChartsCoreOption => {
+    const series = Object.entries(data).map(([name, values], index) => {
+      const prop = get_property_type(obj, name);
+      switch (prop.type) {
+        case 'int':
+        case 'float':
+          return create_line_chart(index, name, prop, values);
+        case 'bool':
+        case 'string':
+        case 'symbol':
+        case 'object':
+          return create_symbol_chart(index, name, prop, values);
+        default:
+          throw new Error(`Unsupported property type for chart: ${get_property_type(obj, name)}`);
       }
-    }, [
+    });
+
+    const row_height = 100 / num_layers; // Percentage height per layer
+
+    return {
+      axisPointer: {
+        link: [{ xAxisIndex: 'all' }]
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      dataZoom: [{
+        type: 'slider',
+        xAxisIndex: Array.from(Array(num_layers).keys()),
+        bottom: 10
+      }],
+      grid: series.map((_, i) => ({
+        left: 30,
+        right: 10,
+        height: `${row_height - 10}%`,
+        top: `${i * row_height + 5}%`
+      })),
+      xAxis: series.map((_, i) => ({
+        type: 'time',
+        gridIndex: i,
+        show: i === num_layers - 1,
+      })),
+      yAxis: series.map((serie) => serie.yAxis),
+      series: series.map((serie) => serie.series)
+    };
+  }
+
+  const obj_listener = {
+    class_added: (_cls: coco.CoCoClass) => { flick.redraw(); },
+    properties_updated: (_properties: Record<string, coco.Value>) => { flick.redraw(); },
+    values_added: (_values: Record<string, coco.Value>, _date_time: string) => { flick.redraw(); if (chart) chart.setOption(get_option()); },
+    data_updated: (_data: Record<string, Array<coco.TimeValue>>) => { if (chart) chart.setOption(get_option()); }
+  };
+
+  let resize_handler: () => void;
+
+  const vals = obj.get_values();
+  const content = h('div.container.mt-2', [
     h('div.input-group', [
       h('input.form-control', { attrs: { type: 'text', value: obj.get_id(), placeholder: 'Type name', disabled: true } }),
       h('button.btn.btn-outline-secondary', {
@@ -101,12 +141,20 @@ export function CoCoObject(obj: coco.CoCoObject): VNode {
       style: { minHeight: `${vals ? (Object.values(vals).length * PIXELS_PER_ROW) + MARGIN_TOP + GAP : 30}px` },
       hook: {
         insert: (vnode) => {
-          chart = new ObjectChart(vnode.elm as HTMLDivElement, obj);
+          chart = echarts.init(vnode.elm as HTMLDivElement);
+          chart.setOption(get_option());
+
+          resize_handler = () => chart?.resize();
+          window.addEventListener('resize', resize_handler);
+
+          obj.add_listener(obj_listener);
         },
         destroy: () => {
+          window.removeEventListener('resize', resize_handler);
+          obj.remove_listener(obj_listener);
           if (chart) {
             chart.dispose();
-            chart = null;
+            chart = undefined;
           }
         }
       }
@@ -119,81 +167,16 @@ function object_to_string(obj: coco.CoCoObject): string {
   return obj.get_properties()?.name as string || obj.get_id();
 }
 
-class ObjectChart implements coco.CoCoObjectListener {
-
-  private readonly chart: echarts.ECharts;
-  private readonly obj: coco.CoCoObject;
-
-  constructor(node: HTMLDivElement, obj: coco.CoCoObject) {
-    this.chart = echarts.init(node);
-    this.obj = obj;
-    this.obj.add_listener(this);
+function get_property_type(obj: coco.CoCoObject, prop_name: string): coco.Property {
+  for (const cls of obj.get_classes().values()) {
+    const prop = cls.get_dynamic_properties().get(prop_name);
+    if (prop) return prop;
   }
-
-  class_added(_cls: coco.CoCoClass): void { }
-  properties_updated(_properties: Record<string, coco.Value>): void { }
-  values_added(_values: Record<string, coco.Value>, _date_time: string): void { }
-  data_updated(data: Record<string, Array<coco.TimeValue>>): void {
-    const n_series = Object.keys(data).length;
-    const series = Object.entries(data).map(([name, values], index) => create_property_chart(this.obj, index, n_series, name, values));
-
-    const options = {
-      tooltip: {
-        trigger: 'axis',
-      },
-      dataZoom: [{ type: 'slider', xAxisIndex: Array.from(Array(n_series).keys()) }],
-      grid: [] as any[],
-      xAxis: [] as any[],
-      yAxis: [] as any[],
-      series: [] as any[],
-    };
-    for (const option of series) {
-      options.grid.push(option.grid);
-      options.xAxis.push(option.xAxis);
-      options.yAxis.push(option.yAxis);
-      options.series.push(option.series);
-    }
-    this.chart.setOption(options);
-  }
-
-  dispose() {
-    this.obj.remove_listener(this);
-    this.chart.dispose();
-  }
+  throw new Error(`Property ${prop_name} not found in object ${obj.get_id()}`);
 }
 
-function create_property_chart(obj: coco.CoCoObject, index: number, total: number, name: string, data: Array<coco.TimeValue>): echarts.EChartsCoreOption {
-  const prop = get_property_type(obj, name);
-  switch (prop.type) {
-    case 'int':
-    case 'float':
-      return create_line_chart(index, total, name, prop, data);
-    case 'bool':
-    case 'string':
-    case 'symbol':
-    case 'object':
-      return create_symbol_chart(index, total, name, prop, data);
-    default:
-      throw new Error(`Unsupported property type for chart: ${get_property_type(obj, name)}`);
-  }
-}
-
-function create_line_chart(index: number, total: number, name: string, prop: coco.Property, data: Array<coco.TimeValue>): echarts.EChartsCoreOption {
-  const top = MARGIN_TOP + (index * PIXELS_PER_ROW);
-  const height = PIXELS_PER_ROW - GAP;
+function create_line_chart(index: number, name: string, prop: coco.Property, data: Array<coco.TimeValue>): echarts.EChartsCoreOption {
   return {
-    grid: {
-      top: top,
-      height: height,
-      left: 30,
-      right: 10
-    },
-    xAxis: {
-      type: 'time',
-      gridIndex: index,
-      show: index === total - 1, // Only bottom chart shows labels
-      axisPointer: { show: true, snap: true, lineStyle: { color: '#999', width: 2 } }
-    },
     yAxis: {
       type: 'value',
       gridIndex: index,
@@ -207,72 +190,27 @@ function create_line_chart(index: number, total: number, name: string, prop: coc
       type: 'line',
       xAxisIndex: index,
       yAxisIndex: index,
-      data: data.map(d => [d.timestamp, d.value])
+      data: data.map(d => [d.timestamp, d.value as number])
     }
   }
 }
 
-function create_symbol_chart(index: number, total: number, name: string, prop: coco.Property, data: Array<coco.TimeValue>): echarts.EChartsCoreOption {
-  const top = MARGIN_TOP + (index * PIXELS_PER_ROW);
-  const height = PIXELS_PER_ROW - GAP;
+function create_symbol_chart(index: number, name: string, prop: coco.Property, data: Array<coco.TimeValue>): echarts.EChartsCoreOption {
   return {
-    grid: {
-      top: top,
-      height: height,
-      left: 30,
-      right: 10
-    },
-    xAxis: {
-      type: 'time',
-      gridIndex: index,
-      show: index === total - 1, // Only bottom chart shows labels
-      axisPointer: { show: true, snap: true, lineStyle: { color: '#999', width: 2 } }
-    },
     yAxis: {
       type: 'value',
       gridIndex: index,
       name,
+      min: prop.type === 'int' ? (prop.min ? prop.min as number - 1 : undefined) : undefined,
+      max: prop.type === 'int' ? (prop.max ? prop.max as number + 1 : undefined) : undefined,
       splitLine: { show: true }
     },
     series: {
       name,
-      type: 'custom',
+      type: 'line',
       xAxisIndex: index,
       yAxisIndex: index,
-      renderItem: (params: CustomSeriesRenderItemParams, api: CustomSeriesRenderItemAPI) => {
-        const coordSys = params.coordSys as unknown as {
-          x: number;
-          y: number;
-          width: number;
-          height: number;
-        };
-
-        const start = api.coord([api.value(0), 0]);
-        const end = api.coord([api.value(1), 0]);
-
-        return {
-          type: 'rect',
-          shape: {
-            x: start[0],
-            y: coordSys.y,
-            width: Math.max(0, end[0] - start[0]), // Ensure width isn't negative
-            height: coordSys.height
-          },
-          style: {
-            fill: api.visual('color')
-          }
-        };
-      },
-      encode: { x: [0, 1], y: 2 },
-      data: data // Ensure this is [[start, end, "VALUE"], ...]
+      data: data.map(d => [d.timestamp, d.value as number])
     }
   }
-}
-
-function get_property_type(obj: coco.CoCoObject, prop_name: string): coco.Property {
-  for (const cls of obj.get_classes().values()) {
-    const prop = cls.get_dynamic_properties().get(prop_name);
-    if (prop) return prop;
-  }
-  throw new Error(`Property ${prop_name} not found in object ${obj.get_id()}`);
 }

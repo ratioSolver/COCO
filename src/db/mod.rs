1434364b208628520a844336b490e83445876851
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-03-31 16:33:47 +0200

    feat: extend Database trait with object and rule management methods; implement MongoDB integration for new functionalities

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,7 @@
-use crate::model::Class;
+use crate::model::{Class, Object, Rule, Value};
 use async_trait::async_trait;
-use std::fmt;
+use chrono::{DateTime, Utc};
+use std::{collections::HashMap, fmt};
 
 #[cfg(feature = "mongodb")]
 mod mongodb;
@@ -37,6 +38,22 @@ pub trait Database: Clone + Send + Sync + 'static {
 
     async fn get_classes(&self) -> Result<Vec<Class>, DatabaseError>;
     async fn create_class(&self, class: Class) -> Result<(), DatabaseError>;
+
+    async fn get_objects(&self) -> Result<Vec<Object>, DatabaseError>;
+    async fn create_object(&self, object: Object) -> Result<String, DatabaseError>;
+    async fn add_class(&self, object_id: String, class_name: String) -> Result<(), DatabaseError>;
+    async fn set_properties(&self, object_id: String, properties: &HashMap<String, Value>) -> Result<(), DatabaseError>;
+    async fn add_data(&self, object_id: String, values: HashMap<String, Value>, date_time: DateTime<Utc>) -> Result<(), DatabaseError>;
+    async fn get_data(&self, object_id: String, start_time: Option<DateTime<Utc>>, end_time: Option<DateTime<Utc>>) -> Result<Vec<(HashMap<String, Value>, DateTime<Utc>)>, DatabaseError>;
+
+    async fn get_rules(&self) -> Result<Vec<Rule>, DatabaseError>;
+    async fn create_rule(&self, rule: Rule) -> Result<(), DatabaseError>;
+
+    async fn add_fcm_token(&self, object_id: String, token: String) -> Result<(), DatabaseError>;
+    async fn remove_fcm_token(&self, object_id: String, token: String) -> Result<(), DatabaseError>;
+    async fn get_fcm_tokens(&self, object_id: String) -> Result<Vec<String>, DatabaseError>;
+
+    async fn drop_database(&self) -> Result<(), DatabaseError>;
 }
 
 pub async fn setup_db() -> Result<impl Database, DatabaseError> {
@@ -55,5 +72,5 @@ async fn setup_mongodb() -> Result<impl Database, DatabaseError> {
     let host = std::env::var("DB_HOST").unwrap_or_else(|_| "localhost".to_owned());
     let port = std::env::var("DB_PORT").ok().and_then(|p| p.parse().ok()).unwrap_or(27017);
     let uri = format!("mongodb://{}:{}", host, port);
-    Ok(MongoDB::new(&name, &uri).await?)
+    MongoDB::new(name, uri).await
 }
